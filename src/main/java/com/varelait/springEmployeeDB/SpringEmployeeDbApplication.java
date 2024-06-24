46586490113eba5caa9c3909cf94a50c8a256106
Ismael Varela <info@varelait.com>
2024-06-24 19:18:52 -0400

    trying to set up spring session jdbc with no success

--- src/main/java/com/varelait/springEmployeeDB/SpringEmployeeDbApplication.java
+++ src/main/java/com/varelait/springEmployeeDB/SpringEmployeeDbApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 @ComponentScan("com.varelait")
+@EnableJdbcHttpSession
 public class SpringEmployeeDbApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/varelait/springEmployeeDB/presentation/user/UserEndpoint.java
+++ src/main/java/com/varelait/springEmployeeDB/presentation/user/UserEndpoint.java
@@ -4,6 +4,7 @@ import com.varelait.springEmployeeDB.service.entities.UserResponse;
 import com.varelait.springEmployeeDB.service.entities.UserDTO;
 import com.varelait.springEmployeeDB.service.user.IUserService;
 import com.varelait.springEmployeeDB.service.user.UserService;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,69 +24,63 @@ public class UserEndpoint {
         this.userService = userService;
     }
 
+    private HttpStatus statusState(Object object){
+        if (object == null)
+            return HttpStatus.BAD_REQUEST;
+        else
+            return HttpStatus.OK;
+    }
+
     @PostMapping("/auth")
-    public ResponseEntity<UserResponse> authenticate(@Valid @RequestBody UserDTO userRequest){
+    public ResponseEntity<UserResponse> authenticate(@Valid @RequestBody UserDTO userRequest, HttpSession session){
         UserResponse user = userService.authenticate(userRequest);
         HttpStatus status = HttpStatus.BAD_REQUEST;
 
         if (user != null)
             status = HttpStatus.OK;
 
+        session.setAttribute("logged", true);
+        session.setAttribute("userEmail", user.email);
+        session.setAttribute("userid", user.id);
         return new ResponseEntity<>(user, status);
     }
 
     @PostMapping
     public ResponseEntity<UserResponse> create(@Valid @RequestBody UserDTO userRequest){
         UserResponse user = userService.create(userRequest);
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-
-        if (user != null)
-            status = HttpStatus.OK;
-
+        HttpStatus status = statusState(user);
         return new ResponseEntity<>(user, status);
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<UserResponse> update(@PathVariable int id, @Valid @RequestBody UserDTO userRequest){
-        var user = userService.update(id, userRequest);
-        HttpStatus status = HttpStatus.BAD_REQUEST;
+    public ResponseEntity<UserResponse> update(@PathVariable int id, @Valid @RequestBody UserDTO userRequest, HttpSession session){
+        UserResponse user = null;
 
-        if (user != null)
-            status = HttpStatus.OK;
+        if ((boolean) session.getAttribute("logged"))
+            user = userService.update(id, userRequest);
 
+        HttpStatus status = statusState(user);
         return new ResponseEntity<>(user, status);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> get(@PathVariable int id){
         var user = userService.get(id);
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-
-        if (user != null)
-            status = HttpStatus.OK;
-
+        HttpStatus status = statusState(user);
         return new ResponseEntity<>(user, status);
     }
 
     @GetMapping()
     public ResponseEntity<List<UserResponse>> get(){
         var user = userService.get();
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-
-        if (!user.isEmpty())
-            status = HttpStatus.OK;
-
+        HttpStatus status = statusState(user);
         return new ResponseEntity<>(user, status);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<UserResponse> delete(@PathVariable int id){
         var user = userService.delete(id);
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-
-        if (user != null)
-            status = HttpStatus.OK;
-
+        HttpStatus status = statusState(user);
         return new ResponseEntity<>(user, status);
     }
 

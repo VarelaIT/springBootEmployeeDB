e507befa98a17e918444a8ed1480dd1c794439bb
Ismael Varela <info@varelait.com>
2024-07-13 18:11:20 -0400

    enable spring security

--- src/main/java/com/varelait/springEmployeeDB/SpringEmployeeDbApplication.java
+++ src/main/java/com/varelait/springEmployeeDB/SpringEmployeeDbApplication.java
@@ -2,11 +2,10 @@ package com.varelait.springEmployeeDB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication()
 @ComponentScan("com.varelait")
 @EnableTransactionManagement
 public class SpringEmployeeDbApplication {
